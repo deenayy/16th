c1805b2cbda094b0828d85be95e617a096d626ee
deenayy <akbargod077@gmail.com>
2024-12-22 16:02:12 +0700

    Update gift.js

--- script/gift.js
+++ script/gift.js
@@ -14,11 +14,6 @@ const shakeSounds = [
     'audio/shake_3.mp3'
 ];
 
-// Memastikan semua audio dipreload
-shakeSounds.forEach(audio => {
-    audio.load();
-});
-
 function playRandomShakeSound() {
     const randomIndex = Math.floor(Math.random() * shakeSounds.length);
     const audio = new Audio(shakeSounds[randomIndex]);
